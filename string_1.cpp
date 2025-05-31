cd9fc7935ecd23f76d764adc214c1878fc45dad7
iamrahatkabir <iftyrahatkabir@gmail.com>
2025-06-01 05:22:06 +0600

    String in CPP

---
+++ string_1.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // char s[10] = "Hello";
+    // strcpy(s, "Gello");
+
+
+    // string s = "Hello";
+    // s = "Gello";
+
+
+    // Old C version
+    // char s[101] = "Hello";
+    // char s2[102] = "Gello";
+
+    // if(strcmp(s,s2) == 0)
+    // {
+    //     cout << "Same" << endl;
+    // }
+    // else{
+    //     cout << "Not Same" << endl;
+    // }
+
+    //New Vesrion in C++;
+    // string s = "Hello";
+    // string s2 = "Gello";
+
+    // if(s == s2)
+    // {
+    //     cout << "Same" << endl;
+    // }
+    // else{
+    //     cout << "Not Same" << endl;
+    // }
+
+    // string can increase its size;
+    string s = "Hello";
+    s = "Hi how are you, How is your day going?";
+
+    cout << s << endl;
+
+
+    
+    return 0;
+}
